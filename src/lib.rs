1492952f7aa9a90558564c8882e6986b423261a2
Lander Brandt <landerbrandt@gmail.com>
2015-10-23 11:55:31 -0700

    Ignore .swp files, add some simple tcpip stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
 #[test]
 fn it_works() {
 }
+
+struct TcpAddr<'a> {
+    host: &'a str,
+    port: u32,
+}
+
+impl<'a> TcpAddr<'a> {
+    fn new(host: &str, port: u32) -> Result<TcpAddr, &'static str> {
+        if port > u16::max_value() as u32 {
+            return Err("port out of range")
+        }
+
+        Ok(TcpAddr{ host: host, port: port})
+    }
+}
+
+#[test]
+fn test_tcp_adr_parse() {
+    match TcpAddr::new("127.0.0.1", u16::max_value() as u32 + 1) {
+        Ok(addr)       =>  {
+            println!("Works!");
+            assert!(addr.host == "127.0.0.1");
+        },
+        Err(fail)   => println!("Fail: {}", fail),
+    }
+}
+
+
